423b0cf2d2098e339d1d4b121b9db7ccfc56fc44
Prateek Bisht <prateekbisht04@gmail.com>
2025-10-23 23:20:22 +0530

    setup the frontend part of the project using vite and integrated tailwindcss and tried keeping all the progress

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div className='bg-[linear-gradient(to_bottom,#A8E6CF,#FFFFFF,#C398E5)] min-h-screen'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Sidebar from '../components/sidebar'
+
+const Home = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Home
